738233219499939e1b699d89ee96274b834ca273
Ze Wang <zewang@uber.com>
2018-11-05 10:32:31 -0800

    change default cuckoo hash index size

---
+++ utils/env.go
@@ -0,0 +1,45 @@
+package utils
+
+import "os"
+
+type AresEnv string
+
+const (
+	EnvProd    AresEnv = "production"
+	EnvStaging AresEnv = "staging"
+	EnvDev     AresEnv = "development"
+	EnvTest    AresEnv = "test"
+)
+
+func GetAresEnv() AresEnv {
+	aresEnv := AresEnv(os.Getenv("ARES_ENV"))
+	switch aresEnv {
+	case EnvProd:
+		break
+	case EnvStaging:
+		break
+	case EnvDev:
+		break
+	case EnvTest:
+		break
+	default:
+		return EnvDev
+	}
+	return aresEnv
+}
+
+func IsTest() bool{
+	return GetAresEnv() == EnvTest
+}
+
+func IsDev() bool{
+	return GetAresEnv() == EnvDev
+}
+
+func IsProd() bool{
+	return GetAresEnv() == EnvProd
+}
+
+func IsStaging() bool{
+	return GetAresEnv() == EnvStaging
+}
