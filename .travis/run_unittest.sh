98acf27c3bd539c450561484b58e21a9b9ec3577
Jian Shen <jians@uber.com>
2018-12-04 13:37:13 -0800

    test cache lib

--- .travis/run_unittest.sh
+++ .travis/run_unittest.sh
@@ -1,8 +1,33 @@
 #!/usr/bin/env bash
 set -ex
 
-find lib -type f  -exec touch {} +
-find gtest -type f  -exec touch {} +
+cachedLibCommit=$(cat lib/.cached_commit 2>/dev/null)
+cudaFileChanged=false
+if [ ! -z "${cachedLibCommit}" ]; then
+  changefiles=$(git diff "${cachedLibCommit}")
+  for file in ${changefiles}
+  do
+    ext="${file##*.}"
+    if [ "$ext" == "cu" ] || [ "$ext" == "h" ]; then
+      echo "c file changed from cacheLibCommit, need to rebuild lib"
+      cudaFileChanged=true	
+      break
+    fi
+  done
+else
+  cudaFileChanged=true
+fi
+
+if [ "${cudaFileChanged}" == "true" ]; then
+  # clean up lib and cuda test when cuda file change found
+	make clean
+  make clean-cuda-test
+else
+  # touch files in lib and gtest to update the timestamp so that make will not treat lib objects as outdated 
+	find lib -type f  -exec touch {} +
+	find gtest -type f  -exec touch {} +
+fi
+
 # run test-cuda in host mode
 make test-cuda -j
 
