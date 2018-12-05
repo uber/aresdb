ca29a4ee1825ebf545a6fb72559400dc35776565
Jian Shen <shenj.soso@gmail.com>
2018-12-04 17:10:09 -0800

    cache lib and gtest (#34)
    * cache lib and gtest folder
    * use cached lib while no cpp, hpp, h file change

--- .travis/run_unittest.sh
+++ .travis/run_unittest.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
 set -ex
+
+if [ -f "lib/.cached_commit" ]; then
+  cachedLibCommit=$(cat lib/.cached_commit)
+fi
+
+cudaFileChanged=false
+if [ ! -z "${cachedLibCommit}" ]; then
+  numCUDAFilesChanged=$(git diff "${cachedLibCommit}" --name-only | grep -c -e "\\.hpp" -e "\\.h" -e "\\.cu" || true)
+  if [ "${numCUDAFilesChanged}" -gt 0 ]; then
+    cudaFileChanged=true
+  fi
+else
+  cudaFileChanged=true
+fi
+
+
+if [ "${cudaFileChanged}" == "true" ]; then
+  # clean up lib and cuda test when cuda file change found
+  make clean
+  make clean-cuda-test
+else
+  # touch files in lib and gtest to update the timestamp so that make will not treat lib objects as outdated 
+  find lib -type f -exec touch {} +
+  find gtest -type f -exec touch {} +
+fi
+
 # run test-cuda in host mode
 make test-cuda -j
 
@@ -8,3 +34,9 @@ make ares -j
 
 # run test
 ginkgo -r -cover
+
+# update cached_commit
+if [ "${cudaFileChanged}" == "true" ]; then
+  currentCommit="$(git rev-list --no-merges -n 1 HEAD)"
+  echo "${currentCommit}" > lib/.cached_commit
+fi
