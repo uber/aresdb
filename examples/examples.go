fe1a88700dc8cd77f7dcd4e657ff36da1f6a874e
Jian Shen <jians@uber.com>
2019-02-07 11:42:22 -0800

    example dataset and queries for aresdb docker

--- examples/examples.go
+++ examples/examples.go
@@ -11,18 +11,56 @@ import (
 	"path/filepath"
 	"strings"
 
+	"bytes"
+	"encoding/json"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/uber-go/tally"
 	"github.com/uber/aresdb/client"
 	"github.com/uber/aresdb/utils"
 	"go.uber.org/zap"
+	"strconv"
+	"time"
 )
 
 const (
-	hostPort   = "localhost:9374"
 	schemaDir  = "schema"
+	queriesDir = "queries"
 	dataDir    = "data"
-	randomTime = "{time}"
+
+	dataSetKeyName = "dataset"
+	hostKeyName    = "host"
+	portKeyName    = "port"
 )
+
+var (
+	random        = rand.New(rand.NewSource(0))
+	now           = utils.Now()
+	unitToSeconds = map[string]time.Duration{
+		"m": time.Minute,
+		"h": time.Hour,
+		"d": 24 * time.Hour,
+	}
+
+	logger = zap.NewExample().Sugar()
+)
+
+func parseAndGenerateRandomTime(timeStr string) uint32 {
+	timeStr = strings.TrimSpace(strings.TrimSuffix(strings.TrimPrefix(timeStr, "{"), "}"))
+
+	t, err := strconv.Atoi(timeStr[:len(timeStr)-1])
+	panicIfErr(err)
+
+	unit, ok := unitToSeconds[timeStr[len(timeStr)-1:]]
+	if !ok {
+		panic(fmt.Sprintf("unit %s not supported", timeStr[len(timeStr)-1:]))
+	}
+
+	duration := time.Duration(t) * unit
+	start := now.Add(-duration)
+
+	return uint32(start.Unix() + random.Int63n(int64(duration.Seconds())))
+}
 
 func panicIfErr(err error) {
 	if err != nil {
@@ -30,7 +68,21 @@ func panicIfErr(err error) {
 	}
 }
 
-func createTablesForDataSet(dataSetName string) {
+func queryDataSet() {
+	dataSetName := viper.GetString(dataSetKeyName)
+	dataSetQueriesDir := fmt.Sprintf("%s/%s", dataSetName, queriesDir)
+	queriesDirInfo, err := ioutil.ReadDir(dataSetQueriesDir)
+	panicIfErr(err)
+	for _, queryInfo := range queriesDirInfo {
+		baseName := queryInfo.Name()
+		queryName := strings.TrimSuffix(baseName, filepath.Ext(baseName))
+		queryPath := fmt.Sprintf("%s/%s/%s", dataSetName, queriesDir, baseName)
+		makeQuery(queryName, queryPath)
+	}
+}
+
+func createTablesForDataSet() {
+	dataSetName := viper.GetString(dataSetKeyName)
 	dataSetSchemaDir := fmt.Sprintf("%s/%s", dataSetName, schemaDir)
 	schemaDirInfo, err := ioutil.ReadDir(dataSetSchemaDir)
 	panicIfErr(err)
@@ -42,30 +94,17 @@ func createTablesForDataSet(dataSetName string) {
 	}
 }
 
-func createTable(tableName string, tableSchemaPath string) {
-	schemaFile, err := os.Open(tableSchemaPath)
-	panicIfErr(err)
-
-	schemaCreationURL := fmt.Sprintf("http://%s/schema/tables", hostPort)
-	rsp, err := http.Post(schemaCreationURL, "application/json", schemaFile)
-	panicIfErr(err)
-
-	if rsp.StatusCode != http.StatusOK {
-		panic(fmt.Sprintf("schema creation failed with status code %d", rsp.StatusCode))
-	}
-	fmt.Printf("table %s created\n", tableName)
-}
-
-func ingestDataForDataSet(dataSetName string) {
+func ingestDataForDataSet() {
+	dataSetName := viper.GetString(dataSetKeyName)
 	dataFileDir := fmt.Sprintf("./%s/%s", dataSetName, dataDir)
 	dataFiles, err := ioutil.ReadDir(dataFileDir)
 	panicIfErr(err)
 
 	cfg := client.ConnectorConfig{
-		Address: hostPort,
+		Address: fmt.Sprintf("%s:%d", viper.GetString(hostKeyName), viper.GetInt(portKeyName)),
 	}
 
-	connector, err := cfg.NewConnector(zap.NewExample().Sugar(), tally.NoopScope)
+	connector, err := cfg.NewConnector(logger, tally.NoopScope)
 	panicIfErr(err)
 
 	for _, dataFileInfo := range dataFiles {
@@ -74,6 +113,40 @@ func ingestDataForDataSet(dataSetName string) {
 		tableName := strings.TrimSuffix(baseName, filepath.Ext(baseName))
 		ingestDataForTable(connector, tableName, dataFilePath)
 	}
+}
+
+func makeQuery(queryName, queryPath string) {
+	file, err := os.Open(queryPath)
+	panicIfErr(err)
+
+	fmt.Printf("making query %s\n ...", queryName)
+	resp, err := http.Post(fmt.Sprintf("http://%s:%d/query/aql", viper.GetString(hostKeyName), viper.GetInt(portKeyName)), "application/json", file)
+	panicIfErr(err)
+	if resp.StatusCode != http.StatusOK {
+		panic(fmt.Errorf("query failed with status code %d", resp.StatusCode))
+	}
+
+	result, err := ioutil.ReadAll(resp.Body)
+	panicIfErr(err)
+
+	prettJSON := &bytes.Buffer{}
+	panicIfErr(json.Indent(prettJSON, result, "", "\t"))
+
+	fmt.Println("Result: \n", prettJSON.String())
+}
+
+func createTable(tableName string, tableSchemaPath string) {
+	schemaFile, err := os.Open(tableSchemaPath)
+	panicIfErr(err)
+
+	schemaCreationURL := fmt.Sprintf("http://%s/schema/tables", fmt.Sprintf("%s:%d", viper.GetString(hostKeyName), viper.GetInt(portKeyName)))
+	rsp, err := http.Post(schemaCreationURL, "application/json", schemaFile)
+	panicIfErr(err)
+
+	if rsp.StatusCode != http.StatusOK {
+		panic(fmt.Sprintf("schema creation failed with status code %d", rsp.StatusCode))
+	}
+	fmt.Printf("table %s created\n", tableName)
 }
 
 func ingestDataForTable(connector client.Connector, tableName string, dataPath string) {
@@ -88,14 +161,12 @@ func ingestDataForTable(connector client.Connector, tableName string, dataPath s
 	rows := make([]client.Row, 0)
 	var record []string
 
-	now := utils.Now().Unix()
-	r := rand.New(rand.NewSource(0))
 	for record, err = csvReader.Read(); err != io.EOF; record, err = csvReader.Read() {
 		panicIfErr(err)
 		row := make(client.Row, 0, len(record))
 		for _, value := range record {
-			if value == randomTime {
-				row = append(row, getRandomEpochSeconds(r, now-86400, now))
+			if strings.HasPrefix(value, "{") && strings.HasSuffix(value, "}") {
+				row = append(row, parseAndGenerateRandomTime(value))
 			} else {
 				row = append(row, value)
 			}
@@ -107,11 +178,42 @@ func ingestDataForTable(connector client.Connector, tableName string, dataPath s
 	fmt.Printf("%d rows inserted into %s\n", rowsInserted, tableName)
 }
 
-func getRandomEpochSeconds(r *rand.Rand, start, end int64) uint32 {
-	return uint32(start + r.Int63n(end-start))
-}
-
 func main() {
-	createTablesForDataSet("1k_trips")
-	ingestDataForDataSet("1k_trips")
+	rootCmd := &cobra.Command{
+		Use:     "examples",
+		Short:   "AresDB Examples",
+		Long:    `AresDB Examples Contains examples for interact with aresdb`,
+		Example: `./examples`,
+	}
+	rootCmd.PersistentFlags().String(dataSetKeyName, "1k_trips", "name for data set")
+	rootCmd.PersistentFlags().String(hostKeyName, "localhost", "host of aresdb server")
+	rootCmd.PersistentFlags().Int(portKeyName, 9374, "port of aresdb server")
+	viper.SetDefault(dataSetKeyName, "1k_trips")
+	viper.SetDefault(hostKeyName, "localhost")
+	viper.SetDefault(portKeyName, 9374)
+	viper.BindPFlags(rootCmd.PersistentFlags())
+
+	dataCmd := &cobra.Command{
+		Use: "data",
+		Run: func(cmd *cobra.Command, args []string) {
+			ingestDataForDataSet()
+		},
+	}
+
+	tableCmd := &cobra.Command{
+		Use: "tables",
+		Run: func(cmd *cobra.Command, args []string) {
+			createTablesForDataSet()
+		},
+	}
+
+	queryCmd := &cobra.Command{
+		Use: "query",
+		Run: func(cmd *cobra.Command, args []string) {
+			queryDataSet()
+		},
+	}
+
+	rootCmd.AddCommand(tableCmd, dataCmd, queryCmd)
+	rootCmd.Execute()
 }
