c34fc7c86e805413689ca175d6f828792c918a9e
Ze Wang <zewang@uber.com>
2019-02-22 15:16:40 -0800

    fix travis

--- .travis/install_clang_tools.sh
+++ .travis/install_clang_tools.sh
@@ -2,7 +2,9 @@
 set -ex
 # install google test
 output_dir=./build
+cmake_version=3.12.0
 rm -rf /tmp/googletest
+
 git clone -b release-1.8.1 --single-branch https://github.com/google/googletest.git /tmp/googletest
 pushd .
 cd /tmp/googletest
@@ -19,3 +21,11 @@ sudo apt-get install cppcheck
 # install cpplint
 sudo apt-get install python-pip
 sudo pip install cpplint
+
+# install cmake
+pushd .
+cd /tmp
+wget https://github.com/Kitware/CMake/releases/download/v${cmake_version}/cmake-${cmake_version}-Linux-x86_64.tar.gz
+tar xzf cmake-${cmake_version}-Linux-x86_64.tar.gz
+popd
+mv /tmp/cmake-${cmake_version}-Linux-x86_64 ./build/cmake
