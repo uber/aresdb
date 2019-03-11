852d4f9ed12e967e6135a7f3a968257d7403a981
Li Ning <lxning@uber.com>
2019-03-11 11:07:05 -0700

    add service config test

--- subscriber/config/service_config_test.go
+++ subscriber/config/service_config_test.go
@@ -12,7 +12,7 @@ import (
 var _ = Describe("service_config", func() {
 	ActiveAresNameSpace = "dev01"
 	ActiveJobNameSpace = "job-test"
-	It("NewServiceConfig", func() {
+	It("NewServiceConfig normal", func() {
 		cfg, err := config.NewYAMLProviderFromFiles("test.yaml")
 		Ω(err).Should(BeNil())
 
@@ -27,6 +27,68 @@ var _ = Describe("service_config", func() {
 
 		res, err := NewServiceConfig(p)
 		Ω(err).Should(BeNil())
+		Ω(res).ShouldNot(BeNil())
+	})
+	It("NewServiceConfig local", func() {
+		cfg, err := config.NewYAMLProviderFromFiles("test-controller-disable.yaml")
+		Ω(err).Should(BeNil())
+
+		p := Params{
+			Environment: utils.EnvironmentContext{
+				Deployment: "test",
+			},
+			Logger: zap.NewNop(),
+			Scope:  tally.NoopScope,
+			Config: cfg,
+		}
+
+		res, err := NewServiceConfig(p)
+		Ω(err).Should(BeNil())
+		Ω(res).ShouldNot(BeNil())
+	})
+	It("NewServiceConfig ares-ns-empty", func() {
+		cfg, err := config.NewYAMLProviderFromFiles("test-ares-ns-empty.yaml")
+		Ω(err).Should(BeNil())
+
+		p := Params{
+			Environment: utils.EnvironmentContext{
+				Deployment: "test",
+			},
+			Logger: zap.NewNop(),
+			Scope:  tally.NoopScope,
+			Config: cfg,
+		}
+
+		res, err := NewServiceConfig(p)
+		Ω(err).ShouldNot(BeNil())
+		Ω(res).ShouldNot(BeNil())
+
+		cfg, err = config.NewYAMLProviderFromFiles("test-ares-cluster-empty.yaml")
+		Ω(err).Should(BeNil())
+
+		p.Config = cfg
+
+		res, err = NewServiceConfig(p)
+		Ω(err).ShouldNot(BeNil())
+		Ω(res).ShouldNot(BeNil())
+
+		cfg, err = config.NewYAMLProviderFromFiles("test-job-empty.yaml")
+		Ω(err).Should(BeNil())
+
+		p.Config = cfg
+
+		res, err = NewServiceConfig(p)
+		Ω(err).ShouldNot(BeNil())
+		Ω(res).ShouldNot(BeNil())
+
+		ActiveAresNameSpace = "dev01-ares"
+		cfg, err = config.NewYAMLProviderFromFiles("test.yaml")
+		Ω(err).Should(BeNil())
+
+		p.Config = cfg
+
+		res, err = NewServiceConfig(p)
+		Ω(err).ShouldNot(BeNil())
 		Ω(res).ShouldNot(BeNil())
 	})
 })
