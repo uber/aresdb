465515e2451077a2e572b824ffc26730ef4cd773
Hongzheng Shi <shz@uber.com>
2019-04-24 10:00:07 -0700

    clean up

--- examples/fo.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open("trips.csv", "rb") as csvfile:
-	with open("trips1.csv", 'wb') as outfile:
-		thereader = csv.reader(csvfile, delimiter=',')
-		thewriter = csv.writer(outfile, delimiter=',')
-		for row in thereader:
-			thewriter.writerow(row + ["Point(11.1 22.2)"])
