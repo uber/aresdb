df2d5ba21855eaacf02e0e50254cfeb737627049
Jian Shen <jians@uber.com>
2019-04-28 08:46:23 -0700

    WIP

--- memstore/redo_log_file_rotator.go
+++ memstore/redo_log_file_rotator.go
@@ -2,78 +2,83 @@ package memstore
 
 import (
 	"github.com/uber/aresdb/utils"
+	"math"
 	"sync"
+	"time"
 )
 
 type PurgeType int
 
 const (
-	// PurgeLatest purges latest redolog file that match purge condition
-	PurgeLatest PurgeType = iota
-	// PurgeAll purges all redolog files that match purge condition
+	// PurgeByOrder purges files match purge condition according to creation order
+	PurgeByOrder PurgeType = iota
+	// PurgeAll purges all files that match purge condition
 	PurgeAll
 )
-
 // Purger takes care of purging given fileID
 type Purger func(fileID int64) error
 
-type RotationCondition func(upsertBatch *UpsertBatch, condition *RedologFileRotator) bool
+type RedologFileRotator interface {
+	GetOrCreateRedologFile(batch *UpsertBatch, now time.Time, newFileID int64) (fileID int64, newFile bool)
+	CommitUpsertBatch(fileID int64, upsertBatch *UpsertBatch) (offset int)
+	UpdateMaxEventTime(fileID int64, eventTime uint32)
+	PurgeRedologFiles(eventCutoff uint32, checkPointFileID int64, batchOffset int, purgeType PurgeType, purger Purger) error
+}
 
-// RedologFileRotator
-type RedologFileRotator struct {
+// redologFileRotator is the implementation of RedologFileRotator
+type redologFileRotator struct {
 	sync.RWMutex
 	// BatchCountPerFile
 	BatchCountPerFile map[int64]int `json:"batchCountPerFile"`
 	// MaxEventTimePerFile
 	MaxEventTimePerFile map[int64]uint32 `json:"maxEventTimePerFile"`
 	// SizePerFile
-	SizePerFile map[int64]uint32 `json:"sizePerFile"`
+	SizePerFile      map[int64]uint32 `json:"sizePerFile"`
 	TotalRedoLogSize uint
-	CurrentFileID int64
-	CurrentRedologSize int
+
+	CurrentFileID           *int64
+	CurrentFileCreationTime int64
+	CurrentRedoLogSize         int
+
+	RotationInterval int64
+	MaxRedoLogSize int64
 
 	tableName string
 	shard int
-
-	conditions []RotationCondition
 }
 
-func SizeBasedRotation(maxRedologSize int) RotationCondition {
-	return func(upsertBatch *UpsertBatch, r *RedologFileRotator) bool {
-		return r.CurrentRedologSize+4+len(upsertBatch.buffer) < maxRedologSize
+// GetOrCreateRedologFile add a new upsert batch to the redolog manager and returns the fileID and offset
+func (r *redologFileRotator) GetOrCreateRedologFile(batch *UpsertBatch, now time.Time, newFileID int64) (fileID int64, newFile bool) {
+	upsertBatchSize := len(batch.buffer)
+	if r.CurrentFileID == nil ||
+		now.Unix() >= r.CurrentFileCreationTime+r.RotationInterval ||
+		int64(r.CurrentRedoLogSize+upsertBatchSize+4) >= r.MaxRedoLogSize {
+		newID := newFileID
+		r.CurrentFileID = &newID
+		r.CurrentFileCreationTime = now.Unix()
+		r.MaxEventTimePerFile[*r.CurrentFileID] = 0
+		r.SizePerFile[*r.CurrentFileID] = 0
+		utils.GetReporter(r.tableName, r.shard).GetGauge(utils.CurrentRedologCreationTime).Update(float64(now.Unix()))
+		utils.GetReporter(r.tableName, r.shard).GetGauge(utils.NumberOfRedologs).Update(float64(len(r.SizePerFile)))
+		newFile = true
 	}
+	return *r.CurrentFileID, newFile
 }
 
-func 
-
-func (r *RedologFileRotator) AddUpsertBatch(batch *UpsertBatch, newFileID int64) (fileID, offset int) {
-	// If current file is still valid we just return the writer back.
-	if r.CurrentFileID < 0 {
-		// new file
-	}
-
-	if r.currentLogFile != nil && dataTime < r.CurrentFileCreationTime+r.RotationInterval &&
-		int64(r.CurrentRedoLogSize+upsertBatchSize+4) < r.MaxRedoLogSize {
-		return
-	}
-
-	for _, c := range r.conditions {
-		if c.Check(r) {
-
-		}
-	}
-
-	if _, ok := r.BatchCountPerFile[redoFile]; !ok {
-		r.BatchCountPerFile[redoFile] = 1
+func (r *redologFileRotator) CommitUpsertBatch(fileID int64, upsertBatch *UpsertBatch) (offset int) {
+	if _, ok := r.BatchCountPerFile[*r.CurrentFileID]; !ok {
+		r.BatchCountPerFile[*r.CurrentFileID] = 1
 	} else {
-		r.BatchCountPerFile[redoFile]++
+		r.BatchCountPerFile[*r.CurrentFileID]++
 	}
-
-	return newFileID,
+	r.CurrentRedoLogSize += len(upsertBatch.buffer) + 4
+	r.SizePerFile[*r.CurrentFileID] += uint32(len(upsertBatch.buffer)) + 4
+	r.TotalRedoLogSize += uint(len(upsertBatch.buffer)) + 4
+	return r.BatchCountPerFile[*r.CurrentFileID] - 1
 }
 
 // update max eventime to redolog files
-func (r *RedologFileRotator) UpdateMaxEventTime(fileID int64, eventTime uint32) {
+func (r *redologFileRotator) UpdateMaxEventTime(fileID int64, eventTime uint32) {
 	r.Lock()
 	defer r.Unlock()
 	if _, ok := r.MaxEventTimePerFile[fileID]; ok && eventTime <= r.MaxEventTimePerFile[fileID] {
@@ -83,8 +88,17 @@ func (r *RedologFileRotator) UpdateMaxEventTime(fileID int64, eventTime uint32) 
 	r.MaxEventTimePerFile[fileID] = eventTime
 }
 
-func (r *RedologFileRotator) PurgeRedologFiles(eventCutoff uint32, checkPointFileID int64, batchOffset int, purgeType PurgeType, purger Purger) error {
-	fileIDs := r.getRedoLogFilesToPurge(eventCutoff, checkPointFileID, batchOffset)
+func (r *redologFileRotator) PurgeRedologFiles(eventCutoff uint32, checkPointFileID int64, batchOffset int, purgeType PurgeType, purger Purger) error {
+	var fileIDs []int64
+	switch purgeType {
+	case PurgeByOrder:
+		fileIDs = r.getPurgeableFilesByOrder(eventCutoff, checkPointFileID, batchOffset)
+	case PurgeAll:
+		fallthrough
+	default:
+		fileIDs = r.getAllPurgeableFiles(eventCutoff, checkPointFileID, batchOffset)
+	}
+
 	for _, fileID := range fileIDs {
 		if err := purger(fileID); err != nil {
 			return err
@@ -94,23 +108,51 @@ func (r *RedologFileRotator) PurgeRedologFiles(eventCutoff uint32, checkPointFil
 	return nil
 }
 
-func (r *RedologFileRotator) getRedoLogFilesToPurge(cutoff uint32, redoFileCheckpointed int64, batchOffset int) []int64 {
+// check whether fileID matches purge condition
+// assuming lock already held
+func (r *redologFileRotator) checkPurgeConditionWithLock(fileID int64, maxEventTime, eventCutoff uint32, redoFileCheckpointed int64, batchOffset int) bool {
+	// exclude current redo file since it's used by ingestion
+	return (r.CurrentFileID == nil || fileID != *r.CurrentFileID) &&
+			maxEventTime < eventCutoff &&
+			(fileID < redoFileCheckpointed || (fileID == redoFileCheckpointed && r.BatchCountPerFile[redoFileCheckpointed] == batchOffset+1))
+}
+
+func (r *redologFileRotator) getAllPurgeableFiles(cutoff uint32, redoFileCheckpointed int64, batchOffset int) []int64 {
 	r.RLock()
-	var fileIDs []int64
+	var purgeableFileIDs []int64
 	for fileID, maxEventTime := range r.MaxEventTimePerFile {
-		// exclude current redo file since it's used by ingestion
-		if (fileID < r.CurrentFileID || r.CurrentFileID == 0) && maxEventTime < cutoff {
-			if fileID < redoFileCheckpointed ||
-				(fileID == redoFileCheckpointed && r.BatchCountPerFile[redoFileCheckpointed] == batchOffset+1) {
-				fileIDs = append(fileIDs, fileID)
-			}
+		if r.checkPurgeConditionWithLock(fileID, maxEventTime, cutoff, redoFileCheckpointed, batchOffset) {
+			purgeableFileIDs = append(purgeableFileIDs, fileID)
 		}
 	}
 	r.RUnlock()
-	return fileIDs
+	return purgeableFileIDs
 }
 
-func (r *RedologFileRotator) evictRedoLogData(fileID int64) {
+func (r *redologFileRotator) getPurgeableFilesByOrder(cutoff uint32, redoFileCheckpointed int64, batchOffset int) []int64 {
+	r.RLock()
+	var purgeableFileIDs []int64
+	var firstUnpurgable int64 = math.MaxInt64
+	for fileID, maxEventTime := range r.MaxEventTimePerFile {
+		if r.checkPurgeConditionWithLock(fileID, maxEventTime, cutoff, redoFileCheckpointed, batchOffset) && fileID < firstUnpurgable {
+			purgeableFileIDs = append(purgeableFileIDs, fileID)
+		} else {
+			firstUnpurgable = fileID
+		}
+	}
+	r.RUnlock()
+	// last check for purgeable fileIDs to exclude fileIDs > firstUnpurgable
+	last := 0
+	for i := 0; i < len(purgeableFileIDs); i++ {
+		if purgeableFileIDs[i] < firstUnpurgable {
+			purgeableFileIDs[last], purgeableFileIDs[i] = purgeableFileIDs[i], purgeableFileIDs[last]
+			last++
+		}
+	}
+	return purgeableFileIDs[0:last]
+}
+
+func (r *redologFileRotator) evictRedoLogData(fileID int64) {
 	r.Lock()
 	delete(r.MaxEventTimePerFile, fileID)
 	delete(r.BatchCountPerFile, fileID)
