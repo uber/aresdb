45a737119920899e01296127d05d5c0d24f16d94
Ze Wang <zewang@uber.com>
2019-05-29 16:47:19 -0700

    migrate to go mod

--- .travis/pre_install.sh
+++ .travis/pre_install.sh
@@ -8,3 +8,8 @@ add-apt-repository "deb [arch=amd64] https://packages.confluent.io/deb/5.1 stabl
 apt-get update
 apt-get install -y librdkafka-dev
 
+# a hack to overwrite /usr/include/librdkafka/rdkafka.h
+# since "RD_KAFKA_RESP_ERR__FATAL" is not present in librdkafka-dev v0.11.6
+# even the confluent kafka claims it requires version >= v0.11.5
+sudo cp -rf thirdparty/librdkafka/src/rdkafka.h /usr/include/librdkafka/rdkafka.h
+
