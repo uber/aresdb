bbc5423f19c20868247f71fdd024793053ece67a
Ze Wang <zewang@uber.com>
2019-05-29 15:39:35 -0700

    migrate to go mod

--- .travis/install_golang_tools.sh
+++ .travis/install_golang_tools.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
-go get github.com/axw/gocov/...
+go get github.com/axw/gocov/gocov
 go get github.com/AlekSi/gocov-xml
 go get -u golang.org/x/lint/golint
 go get -u github.com/onsi/ginkgo/ginkgo
