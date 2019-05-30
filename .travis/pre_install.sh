e2a10272e3fd1f03f60e44a8e6976e227cb5a2cd
Ze Wang <zewang@uber.com>
2019-05-30 14:30:30 -0700

    fix tests

--- .travis/pre_install.sh
+++ .travis/pre_install.sh
@@ -4,5 +4,6 @@ apt-get update -q
 # a hack to overwrite /usr/include/librdkafka/rdkafka.h
 # since "RD_KAFKA_RESP_ERR__FATAL" is not present in librdkafka-dev v0.11.6
 # even the confluent kafka claims it requires version >= v0.11.5
+mkdir -p /usr/include/librdkafka
 cp -rf thirdparty/librdkafka/src/rdkafka.h /usr/include/librdkafka/rdkafka.h
 
