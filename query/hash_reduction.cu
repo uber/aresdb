394ef33ebdd15d854167715b8b1921450f7c1fef
Ze Wang <zewang@uber.com>
2019-06-11 11:47:29 -0700

    fix tests

--- query/hash_reduction.cu
+++ query/hash_reduction.cu
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 #ifdef SUPPORT_HASH_REDUCTION
-#include <groupby/aggregation_operations.hpp>
 #include <hash/concurrent_unordered_map.cuh>
 #include <hash/hash_functions.cuh>
 #endif
@@ -22,6 +21,9 @@
 #include <cstring>
 #include <algorithm>
 #include <exception>
+#ifdef SUPPORT_HASH_REDUCTION
+#include <groupby/aggregation_operations.hpp>
+#endif
 #include "algorithm.hpp"
 #include "iterator.hpp"
 #include "memory.hpp"
