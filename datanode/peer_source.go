222bb8800005fe720d9a890f5c85353b9d057456
Jian Shen <jians@uber.com>
2019-06-16 15:01:02 -0700

    add tests

---
+++ datanode/peer_source.go
@@ -0,0 +1,164 @@
+package datanode
+
+import (
+	"github.com/pkg/errors"
+	"github.com/uber/aresdb/cluster/topology"
+	"github.com/uber/aresdb/datanode/client"
+	"github.com/uber/aresdb/datanode/generated/proto/rpc"
+	"github.com/uber/aresdb/utils"
+	"google.golang.org/grpc"
+	"sync"
+)
+
+var (
+	errPeerClosed   = errors.New("peer closed")
+	errPeerNotExist = errors.New("peer does not exist")
+)
+
+type peer struct {
+	sync.RWMutex
+	sync.WaitGroup
+
+	host topology.Host
+
+	conn   *grpc.ClientConn
+	closed bool
+}
+
+func (p *peer) Host() topology.Host {
+	return p.host
+}
+
+func (p *peer) BorrowConnection(fn client.WithConnectionFn) (err error) {
+	p.Lock()
+	if p.closed {
+		p.Unlock()
+		return errPeerClosed
+	}
+
+	if p.conn == nil {
+		p.conn, err = grpc.Dial(p.host.Address(), grpc.WithInsecure())
+		if err != nil {
+			p.Unlock()
+			return err
+		}
+	}
+
+	conn := p.conn
+	p.Add(1)
+	p.Unlock()
+
+	fn(rpc.NewPeerDataNodeClient(conn))
+	p.Done()
+	return nil
+}
+
+func (p *peer) Close() {
+	utils.GetLogger().With("host", p.host.String()).Info("closing peer connection")
+	// waiting for on going operation with client connection
+	p.Wait()
+
+	p.Lock()
+	defer p.Unlock()
+	if p.conn != nil {
+		p.conn = nil
+		p.closed = true
+		err := p.conn.Close()
+		if err != nil {
+			utils.GetLogger().With("host", p.host.String(), "error", err.Error()).Error("failed to close grpc connection")
+		}
+	}
+}
+
+func NewPeer(host topology.Host) *peer {
+	return &peer{
+		host: host,
+	}
+}
+
+type peerSource struct {
+	sync.RWMutex
+
+	watch topology.MapWatch
+	peers map[string]*peer
+
+	done chan struct{}
+}
+
+func (ps *peerSource) BorrowConnection(hostID string, fn client.WithConnectionFn) error {
+	ps.RLock()
+	peer, exist := ps.peers[hostID]
+	if !exist {
+		ps.RUnlock()
+		return errPeerNotExist
+	}
+	ps.RUnlock()
+
+	return peer.BorrowConnection(fn)
+}
+
+func (ps *peerSource) watchTopoChange() {
+	for {
+		select {
+		case <-ps.watch.C():
+			ps.updateTopoMap(ps.watch.Get())
+		case <-ps.done:
+			return
+		}
+	}
+}
+
+func (ps *peerSource) Close() {
+	close(ps.done)
+
+	ps.Lock()
+	defer ps.Unlock()
+	for _, peer := range ps.peers {
+		peer.Close()
+	}
+}
+
+func (ps *peerSource) updateTopoMap(topoMap topology.Map) {
+	ps.Lock()
+	defer ps.Unlock()
+
+	// unknown host
+	knownHosts := make(map[string]struct{})
+	for _, host := range topoMap.Hosts() {
+		knownHosts[host.ID()] = struct{}{}
+		if _, exist := ps.peers[host.ID()]; !exist {
+			ps.peers[host.ID()] = NewPeer(host)
+		}
+	}
+
+	for hostID, peer := range ps.peers {
+		if _, known := knownHosts[hostID]; !known {
+			delete(ps.peers, hostID)
+			go func() {
+				peer.Close()
+			}()
+		}
+	}
+}
+
+// NewPeerSource creates PeerSource
+func NewPeerSource(topo topology.Topology) (client.PeerSource, error) {
+	mapWatch, err := topo.Watch()
+	if err != nil {
+		return nil, err
+	}
+
+	<-mapWatch.C()
+	topoMap := mapWatch.Get()
+
+	ps := &peerSource{
+		watch: mapWatch,
+		done:  make(chan struct{}),
+		peers: make(map[string]*peer),
+	}
+
+	ps.updateTopoMap(topoMap)
+	go ps.watchTopoChange()
+
+	return ps, nil
+}
