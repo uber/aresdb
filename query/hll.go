40409ee0c864dc5de63973887d209981e0d4aa80
Hongzheng Shi <shz@uber.com>
2019-07-19 15:42:34 -0700

    move

--- query/hll.go
+++ query/hll.go
@@ -79,12 +79,6 @@ func (r *HLLQueryResults) GetBytes() []byte {
 	return r.buffer.Bytes()
 }
 
-// HLLDataWriter is the struct to serialize HLL Data struct.
-type HLLDataWriter struct {
-	queryCom.HLLData
-	buffer []byte
-}
-
 // SerializeHLL allocates buffer based on the metadata and then serializes hll data into the buffer.
 func (qc *AQLQueryContext) SerializeHLL(dataTypes []memCom.DataType,
 	enumDicts map[int][]string, timeDimensions []int) ([]byte, error) {
@@ -92,7 +86,7 @@ func (qc *AQLQueryContext) SerializeHLL(dataTypes []memCom.DataType,
 	paddedRawDimValuesVectorLength := (uint32(dimValResVectorSize(oopkContext.ResultSize, oopkContext.NumDimsPerDimWidth)) + 7) / 8 * 8
 	paddedCountLength := uint32(2*oopkContext.ResultSize+7) / 8 * 8
 	paddedHLLVectorLength := (qc.OOPK.hllVectorSize + 7) / 8 * 8
-	builder := HLLDataWriter{
+	builder := queryCom.HLLDataWriter{
 		HLLData: queryCom.HLLData{
 			ResultSize:                     uint32(oopkContext.ResultSize),
 			NumDimsPerDimWidth:             oopkContext.NumDimsPerDimWidth,
@@ -105,21 +99,21 @@ func (qc *AQLQueryContext) SerializeHLL(dataTypes []memCom.DataType,
 	}
 
 	headerSize, totalSize := builder.CalculateSizes()
-	builder.buffer = make([]byte, totalSize)
+	builder.Buffer = make([]byte, totalSize)
 	if err := builder.SerializeHeader(); err != nil {
 		return nil, err
 	}
 
 	// Copy dim values vector from device.
-	dimVectorH := unsafe.Pointer(&builder.buffer[headerSize])
+	dimVectorH := unsafe.Pointer(&builder.Buffer[headerSize])
 	asyncCopyDimensionVector(dimVectorH, oopkContext.currentBatch.dimensionVectorD[0].getPointer(),
 		oopkContext.ResultSize, 0, oopkContext.NumDimsPerDimWidth, oopkContext.ResultSize, oopkContext.currentBatch.resultCapacity,
 		cgoutils.AsyncCopyDeviceToHost, qc.cudaStreams[0], qc.Device)
 
-	cgoutils.AsyncCopyDeviceToHost(unsafe.Pointer(&builder.buffer[headerSize+paddedRawDimValuesVectorLength]),
+	cgoutils.AsyncCopyDeviceToHost(unsafe.Pointer(&builder.Buffer[headerSize+paddedRawDimValuesVectorLength]),
 		oopkContext.hllDimRegIDCountD.getPointer(), oopkContext.ResultSize*2, qc.cudaStreams[0], qc.Device)
 
-	cgoutils.AsyncCopyDeviceToHost(unsafe.Pointer(&builder.buffer[headerSize+paddedRawDimValuesVectorLength+paddedCountLength]),
+	cgoutils.AsyncCopyDeviceToHost(unsafe.Pointer(&builder.Buffer[headerSize+paddedRawDimValuesVectorLength+paddedCountLength]),
 		oopkContext.hllVectorD.getPointer(), int(qc.OOPK.hllVectorSize), qc.cudaStreams[0], qc.Device)
 	cgoutils.WaitForCudaStream(qc.cudaStreams[0], qc.Device)
 
@@ -165,90 +159,5 @@ func (qc *AQLQueryContext) SerializeHLL(dataTypes []memCom.DataType,
 		}
 	}
 
-	return builder.buffer, nil
-}
-
-// SerializeHeader serialize HLL header
-//	-----------query result 0-------------------
-//	 <header>
-//	 [uint8] num_enum_columns [uint8] bytes per dim ... [padding for 8 bytes]
-//	 [uint32] result_size [uint32] raw_dim_values_vector_length
-//	 [uint8] dim_index_0... [uint8] dim_index_n [padding for 8 bytes]
-//	 [uint32] data_type_0...[uint32] data_type_n [padding for 8 bytes]
-//
-//	 <enum cases 0>
-//	 [uint32_t] number of bytes of enum cases [uint16] column_index [2 bytes: padding]
-//	 <enum values 0> delimited by "\u0000\n" [padding for 8 bytes]
-//
-// 	 <end of header>
-func (builder *HLLDataWriter) SerializeHeader() error {
-	writer := utils.NewBufferWriter(builder.buffer)
-
-	// num_enum_columns
-	if err := writer.AppendUint8(uint8(len(builder.EnumDicts))); err != nil {
-		return err
-	}
-
-	// bytes per dim
-	if err := writer.Append([]byte(builder.NumDimsPerDimWidth[:])); err != nil {
-		return err
-	}
-	writer.AlignBytes(8)
-
-	// result_size
-	if err := writer.AppendUint32(builder.ResultSize); err != nil {
-		return err
-	}
-
-	// raw_dim_values_vector_length
-	if err := writer.AppendUint32(builder.PaddedRawDimValuesVectorLength); err != nil {
-		return err
-	}
-
-	// dim_indexes
-	for _, dimIndex := range builder.DimIndexes {
-		if err := writer.AppendUint8(uint8(dimIndex)); err != nil {
-			return err
-		}
-	}
-	writer.AlignBytes(8)
-
-	// data_types
-	for _, dataType := range builder.DataTypes {
-		if err := writer.AppendUint32(uint32(dataType)); err != nil {
-			return err
-		}
-	}
-	writer.AlignBytes(8)
-
-	// Write enum cases.
-	for columnID, enumCases := range builder.EnumDicts {
-		enumCasesBytes := queryCom.CalculateEnumCasesBytes(enumCases)
-		if err := writer.AppendUint32(enumCasesBytes); err != nil {
-			return err
-		}
-
-		if err := writer.AppendUint16(uint16(columnID)); err != nil {
-			return err
-		}
-
-		// padding
-		writer.SkipBytes(2)
-
-		var enumCaseBytesWritten uint32
-		for _, enumCase := range enumCases {
-			if err := writer.Append([]byte(enumCase)); err != nil {
-				return err
-			}
-
-			if err := writer.Append([]byte(queryCom.EnumDelimiter)); err != nil {
-				return err
-			}
-
-			enumCaseBytesWritten += uint32(len(enumCase)) + 2
-		}
-
-		writer.SkipBytes(int(enumCasesBytes - enumCaseBytesWritten))
-	}
-	return nil
+	return builder.Buffer, nil
 }
