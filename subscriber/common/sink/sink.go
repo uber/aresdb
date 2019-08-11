1a6f4d9ec8417ec71de150c191151ebdd0044653
lxning <lxning@uber.com>
2019-08-10 23:46:05 -0700

    add debug info

--- subscriber/common/sink/sink.go
+++ subscriber/common/sink/sink.go
@@ -21,6 +21,7 @@ import (
 	"github.com/uber/aresdb/subscriber/common/rules"
 	"github.com/uber/aresdb/utils"
 	"math"
+	"reflect"
 	"strings"
 	"unsafe"
 )
@@ -141,77 +142,77 @@ func getDataValue(col interface{}, columnIDInSchema int, jobConfig *rules.JobCon
 		var b bool
 		val.IsBool = true
 		if b, ok = col.(bool); !ok {
-			return val, fmt.Errorf("Invalid bool value, col:%d, val:%v",
-				columnIDInSchema, col)
+			return val, fmt.Errorf("Invalid bool value, col:%d, val:%v, type:%s",
+				columnIDInSchema, col, reflect.TypeOf(col))
 		}
 		val.Valid = true
 		val.BoolVal = b
 	case memCom.Int8:
 		var i8 int8
 		if i8, ok = col.(int8); !ok {
-			return val, fmt.Errorf("Invalid int8 value, col:%d, val:%v",
-				columnIDInSchema, col)
+			return val, fmt.Errorf("Invalid int8 value, col:%d, val:%v, type:%s",
+				columnIDInSchema, col, reflect.TypeOf(col))
 		}
 		val.Valid = true
 		val.OtherVal = unsafe.Pointer(&i8)
 	case memCom.Uint8, memCom.SmallEnum:
 		var ui8 uint8
 		if ui8, ok = col.(uint8); !ok {
-			return val, fmt.Errorf("Invalid uint8 value, col:%d, val:%v",
-				columnIDInSchema, col)
+			return val, fmt.Errorf("Invalid uint8 value, col:%d, val:%v, type:%s",
+				columnIDInSchema, col, reflect.TypeOf(col))
 		}
 		val.Valid = true
 		val.OtherVal = unsafe.Pointer(&ui8)
 	case memCom.Int16:
 		var i16 int16
 		if i16, ok = col.(int16); !ok {
-			return val, fmt.Errorf("Invalid int16 value, col:%d, val:%v",
-				columnIDInSchema, col)
+			return val, fmt.Errorf("Invalid int16 value, col:%d, val:%v, type:%s",
+				columnIDInSchema, col, reflect.TypeOf(col))
 		}
 		val.Valid = true
 		val.OtherVal = unsafe.Pointer(&i16)
 	case memCom.Uint16, memCom.BigEnum:
 		var ui16 uint16
 		if ui16, ok = col.(uint16); !ok {
-			return val, fmt.Errorf("Invalid uint16 value, col:%d, val:%v",
-				columnIDInSchema, col)
+			return val, fmt.Errorf("Invalid uint16 value, col:%d, val:%v, type:%s",
+				columnIDInSchema, col, reflect.TypeOf(col))
 		}
 		val.Valid = true
 		val.OtherVal = unsafe.Pointer(&ui16)
 	case memCom.Int32:
 		var i32 int32
 		if i32, ok = col.(int32); !ok {
-			return val, fmt.Errorf("Invalid int32 value, col:%d, val:%v",
-				columnIDInSchema, col)
+			return val, fmt.Errorf("Invalid int32 value, col:%d, val:%v, type:%s",
+				columnIDInSchema, col, reflect.TypeOf(col))
 		}
 		val.Valid = true
 		val.OtherVal = unsafe.Pointer(&i32)
 	case memCom.Uint32:
 		var ui32 uint32
 		if ui32, ok = col.(uint32); !ok {
-			return val, fmt.Errorf("Invalid uint32 value, col:%d, val:%v",
-				columnIDInSchema, col)
+			return val, fmt.Errorf("Invalid uint32 value, col:%d, val:%v, type:%s",
+				columnIDInSchema, col, reflect.TypeOf(col))
 		}
 		val.Valid = true
 		val.OtherVal = unsafe.Pointer(&ui32)
 	case memCom.Int64:
 		var i64 int64
 		if i64, ok = col.(int64); !ok {
-			return val, fmt.Errorf("Invalid int64 value, col:%d, val:%v",
-				columnIDInSchema, col)
+			return val, fmt.Errorf("Invalid int64 value, col:%d, val:%v, type:%s",
+				columnIDInSchema, col, reflect.TypeOf(col))
 		}
 		val.Valid = true
 		val.OtherVal = unsafe.Pointer(&i64)
 	case memCom.Float32:
 		var f32 float32
 		if f32, ok = col.(float32); !ok {
-			return val, fmt.Errorf("Invalid float32 value, col:%d, val:%v",
-				columnIDInSchema, col)
+			return val, fmt.Errorf("Invalid float32 value, col:%d, val:%v, type:%s",
+				columnIDInSchema, col, reflect.TypeOf(col))
 		}
 		val.Valid = true
 		val.OtherVal = unsafe.Pointer(&f32)
 	default:
-		return val, fmt.Errorf("Invalid data type, col:%d, val:%v, type",
+		return val, fmt.Errorf("Invalid data type, col:%d, val:%v, type:%d",
 			columnIDInSchema, col, dataType)
 	}
 	return val, nil
