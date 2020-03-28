2520802cf3c027e66c77d60ba69d6677ea1f81fd
Jian Shen <jians@uber.com>
2020-03-27 17:10:04 -0700

    fix ginkgo at v1.6.0

--- .travis/install_golang_tools.sh
+++ .travis/install_golang_tools.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 go get github.com/axw/gocov/gocov@v1.0.0
-go get github.com/AlekSi/gocov-xml@v0.0.0-20190121064608-3a14fb1c4737
-go get -u golang.org/x/lint/golint@v0.0.0-20200302205851-738671d3881b
+go get github.com/AlekSi/gocov-xml
+go get -u golang.org/x/lint/golint
 go get -u github.com/onsi/ginkgo/ginkgo@v1.6.0
-go get github.com/modocache/gover@v0.0.0-20171022184752-b58185e213c5
+go get github.com/modocache/gover
 go get github.com/mattn/goveralls@v0.0.5
