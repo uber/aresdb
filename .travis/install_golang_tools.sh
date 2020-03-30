f5e6dc369143b871987fc5934d53154f941d8fd0
Jian Shen <jians@uber.com>
2020-03-30 08:57:28 -0700

    Fix master build (#362)
    * fix master build
    
    * update go version to 1.13.9 for travis build, check in go.sum
    
    * fix version on golang tools
    
    * user go 1.13.9
    
    * fix ginkgo at v1.6.0
    
    * fix travis yaml
    
    * use -j 2 in travis build

--- .travis/install_golang_tools.sh
+++ .travis/install_golang_tools.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ex
-go get github.com/axw/gocov/gocov
+go get github.com/axw/gocov/gocov@v1.0.0
 go get github.com/AlekSi/gocov-xml
 go get -u golang.org/x/lint/golint
-go get -u github.com/onsi/ginkgo/ginkgo
+go get -u github.com/onsi/ginkgo/ginkgo@v1.6.0
 go get github.com/modocache/gover
-go get github.com/mattn/goveralls
+go get github.com/mattn/goveralls@v0.0.5
